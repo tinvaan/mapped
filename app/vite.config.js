473ff9b23d4ff94b72fc21c7e4e9e9b7bf63fb0a
Harish Navnit <harishnavnit@gmail.com>
2024-10-25 02:45:10 +0800

    nuke the old app

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const config = {
-  build: {
-    chunkSizeWarningLimit: 1500
-  },
-  server: {
-    open: true
-  }
-};
-
-export default defineConfig(config);
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
